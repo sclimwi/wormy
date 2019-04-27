1f1b454c72e08f1290278b81c41b25011c8c6ad8
Glenn Hartmann <glennhartmann85@gmail.com>
2019-04-27 13:00:33 -0400

    add ServiceWorker

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// main.js handles service worker registration
+
+// Taken almost verbatin from https://developers.google.com/web/fundamentals/primers/service-workers/
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('sw.js', {
+      scope: '.'
+    }).then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}

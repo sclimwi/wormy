c42b368bea91faf07246f5232a08f22af805d1f2
Glenn Hartmann <glennhartmann85@gmail.com>
2019-04-27 13:40:14 -0400

    .

--- js/main.js
+++ js/main.js
@@ -3,9 +3,7 @@
 // Taken almost verbatin from https://developers.google.com/web/fundamentals/primers/service-workers/
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('sw.js', {
-      scope: '.'
-    }).then(function(registration) {
+    navigator.serviceWorker.register('sw.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {

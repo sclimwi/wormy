de3077030b28d4648c954f0644b0fa503d981bdf
Glenn Hartmann <glennhartmann85@gmail.com>
2019-04-28 17:07:03 -0400

    mostly revert sw.js changes

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 
 var CACHE_VERSION = 1; // update this value when updating the app
 var CACHE_NAME = 'wormy-cache-v' + CACHE_VERSION;
-var localURLsToCache = [
+var urlsToCache = [
   '.',
   'index.html',
   'favicon.ico',
@@ -23,9 +23,6 @@ var localURLsToCache = [
   'js/wormy-levels.js',
   'js/wormy-server.js'
 ];
-var externalURLsToCache = [
-  'https://flackr.github.io/lobby/client/lobby.js'
-]
 
 // install event: is run on first pageload or whenever sw.js is modified. Note
 // that 'installed' service workers are not 'activated' until all current pages
@@ -36,7 +33,7 @@ self.addEventListener('install', function(event) {
     caches.open(CACHE_NAME)
       .then(function(cache) {
         console.log('Opened cache "' + CACHE_NAME + '"');
-        return cache.addAll(localURLsToCache.concat(externalURLsToCache));
+        return cache.addAll(urlsToCache);
       })
   );
 });
@@ -47,36 +44,6 @@ self.addEventListener('install', function(event) {
 // actually do a network fetch, then we cache the result of the fetch for next
 // time.
 self.addEventListener('fetch', function(event) {
-  if (externalURLsToCache.includes(event.request.url)) {
-    event.respondWith(
-      // Try to fetch the external response first.
-      fetch(event.request).then(function(response) {
-        // Check if we received a valid response
-        if (response && response.status == 200) {
-	  // IMPORTANT: Clone the response. A response is a stream
-	  // and because we want the browser to consume the respons
-          // as well as the cache consuming the response, we need
-          // to clone it so we have two streams.
-          var responseToCache = response.clone();
-          caches.open(CACHE_NAME).then(function(cache) {
-	    console.log('Opened cache for "' + event.request.url + '"')
-            cache.put(event.request, responseToCache);
-          });
-          return response;
-        }  
-
-	// Use cache if fetch does not succeed. 
-	return caches.match(event.request).then(function(cachedResponse) {
-          // Cache hits.
-	  if (cachedResponse) {
-            console.log('Returning cached result for "' + event.request.url + '"')
-	    return cachedResponse;
-          }
-          return response;
-	});
-})
-);
-} else {
   event.respondWith(
     caches.match(event.request)
       .then(function(response) {
@@ -110,7 +77,6 @@ self.addEventListener('fetch', function(event) {
         );
       })
     );
-}
 });
 
 // activate event: this is called when the service worker is installed AND the
